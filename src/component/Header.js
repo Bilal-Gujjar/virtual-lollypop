3880a85da98b38de63680e778b183b98e9d3a27d
Bilal Gujjar <bilaltahir515@gmail.com>
2020-11-25 16:34:50 +0500

    Update

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Lolly from '../component/Lolly'
+// import Lolly from '../component/Lolly'
 //import Lolly from '../svg/lolly-image.svg'
 export default function Header() {
   return (
